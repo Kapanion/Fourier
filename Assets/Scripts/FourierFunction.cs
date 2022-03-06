aa59df4d34e0960474f3d21709524027f907205d
kiu_kapanadze.ioane <kapanadze.ioane@kiu.edu.ge>
2022-03-06 17:19:06 +0400

    FourierFunction.cs minor improvements

--- Assets/Scripts/FourierFunction.cs
+++ Assets/Scripts/FourierFunction.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using UnityEngine;
+
 public class FourierFunction
 {
     private Complex[] coefficients;
@@ -11,13 +13,44 @@ public class FourierFunction
     public Complex ValueAt(float t)
     {
         Complex res = Complex.Zero;
-        for(int i = 0; i < coefficients.Length; ++i)
+        for (int i = 0; i < coefficients.Length; ++i)
         {
-            int freq = (i + 1) / 2;
-            if (i % 2 == 0) freq = -freq;
+            int freq = IndexToFrequency(i);
             res += coefficients[i] * Complex.Exp(freq * 2f * System.Math.PI * Complex.ImaginaryOne * t);
         }
         return res;
+    }
+
+    /// <summary>
+    /// Returns the starting rotation of the vector with the rotation frequency of <c>freq</c>.
+    /// </summary>
+    /// <param name="freq"></param>
+    /// <returns></returns>
+    public float GetStartRotation(int freq)
+    {
+        int ind = FrequencyToIndex(freq);
+        return Mathf.Atan2((float)coefficients[ind].Imaginary, (float)coefficients[ind].Real);
+    }
+
+    /// <summary>
+    /// Returns the magnitude of the vector with the rotation frequency of <c>freq</c>.
+    /// </summary>
+    /// <param name="freq"></param>
+    /// <returns></returns>
+    public float GetMagnitude(int freq)
+    {
+        int ind = FrequencyToIndex(freq);
+        return (float)coefficients[ind].Magnitude;
+    }
+
+    private static int IndexToFrequency(int ind)
+    {
+        return ind % 2 == 0 ? -ind/2 : (ind+1)/2;
+    }
+
+    private static int FrequencyToIndex(int freq)
+    {
+        return freq > 0 ? freq * 2 - 1 : -freq * 2;
     }
 
     public static FourierFunction Of(Complex[] vals, int numberOfVectors)
@@ -25,8 +58,7 @@ public class FourierFunction
         Complex[] coeffs = new Complex[numberOfVectors * 2 + 1];
         for (int i = -numberOfVectors; i <= numberOfVectors; ++i)
         {
-            int ind = UnityEngine.Mathf.Abs(i) * 2;
-            if (i > 0) ind--;
+            int ind = FrequencyToIndex(i);
             coeffs[ind] = CalculateCoefficient(vals, i);
             UnityEngine.Debug.Log(i + ": " + coeffs[ind]);
         }
